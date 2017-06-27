ede4ba7bdf484291edab105aaf0e829a5e126729
Rick Brainerd <rickbrainerd12@gmail.com>
2017-06-27 14:37:31 -0400

    Homework 2 - created 3 functions

--- index.js
+++ index.js
@@ -1,24 +1,39 @@
-const ul = document.createElement('ul')
 
-function update(ev){
+
+function handleSubmit(ev){
     ev.preventDefault()
     const f = event.target
     const name = f.personName.value
     const colorName = f.color.value
+    const colorItem = 'aaa'
+    const nameItem = 'aaa'
     const heading = document.querySelector('h1')
-    const ul = document.createElement('ul')
     const stats = document.querySelector('#stats')
-    const nameItem = document.createElement('li')
-    const colorItem = document.createElement('li')
-    nameItem.textContent = name
-    colorItem.textContent = colorName
-    stats.appendChild(ul)
-    ul.appendChild(nameItem)
-    ul.appendChild(colorItem)
-    ul.querySelector('li').style.color = colorName
+    stats.appendChild(renderList(nameItem, name, colorItem, colorName))    
     heading.textContent = name
+}
+
+function renderColor(a) {
+  const colorListItems = a
+  return colorListItems
+}
+
+function renderListItem(a, b) {
+  a = document.createElement('li')
+
+  a.textContent = b
+
+  return a
+}
+
+function renderList(a, b, c, d) {
+  const list = document.createElement('ul')
+  list.appendChild(renderListItem(a, b))
+  list.appendChild(renderListItem(c, d))
+  list.querySelector('li').style.color = renderColor(d)
+  return list
 }
 
 
 const personForm = document.querySelector('#person-form')
-personForm.addEventListener('submit', update)
+personForm.addEventListener('submit', handleSubmit)
